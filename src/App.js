5148548b097e3e6a916274c2de044ade6e32fc46
Dipocbd <dipoojo6@gmail.com>
2022-07-11 11:33:29 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Filter from './components/Filter'
 import Countries from './components/Countries'
 
 function App() {
   return (
-    <>
+    <Router>
     <Header/>
-    <Filter/>
-<Countries/>
-  </>
+<Route exact path="/">
+<Filter/>
+  <Countries/>
+</Route>
+  </Router>
   )
 }
 
