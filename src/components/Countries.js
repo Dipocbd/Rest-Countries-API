d7ddfc7dfc72e5b5b3ce94dd1ed0ad663a9239d7
Dipocbd <dipoojo6@gmail.com>
2022-09-19 16:42:39 -0700

    Addede more data from the rest country api

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,23 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
-const url = "https://restcountries.com/v3.1/all"
+const url = "https://restcountries.com/v2/all"
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
         const fetchCountryData = async ()=>{
             const response = await fetch(url)
             const countries = await response.json()
-            setCountries(countries)
-            console.log(countries);
+            setCountries(countries);
+        
         }
         useEffect(() => {
             fetchCountryData()
 
     }, [])
 
-    const removeCountry = (area) => {
-      const newCountry = countries.filter((country) => country.area !== area)
+    const removeCountry = (numericCode) => {
+      const newCountry = countries.filter((country) => country.numericCode !== numericCode)
       setCountries(newCountry)
     }
     
@@ -25,19 +25,19 @@ const Countries = () => {
     <>
     <section className="grid"> 
     {countries.map((country) =>{
-        const { name, population ,region ,capital, flags, area } = country
+        const { name, population ,region,capital, flags, area, numericCode } = country
 console.log(country);
         return(
-            <article key= {area}>
+            <article key= {numericCode}>
                 <div>
                     <img src={flags.png} alt={name}/>
                     <div className="items">
-                    <h3>{name.official}</h3>
+                    <h3>{name}</h3>
                     <h4>Population:<span>{population}</span></h4>
                     <h4>Region:<span>{region}</span></h4>
-                    {/* <h4>Capital:<span>{capital[0]}</span></h4> */}
+                    <h4>Capital:<span>{capital}</span></h4>
                     <div className='buttons'><Link to={`/countries/${name}`}className="btn">Learn more</Link><button className="btn"
-                     onClick={() => removeCountry(area)}>Remove Country</button>
+                     onClick={() => removeCountry(numericCode)}>Remove Country</button>
                 </div>
                     </div>
                     
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 
 const Country = () => {
+
+const[country, setCountry] = useState([])
+const {name} = useParams()
+
+ useEffect(() =>{
+  const fetchCountry = async ()=>{
+    const response = await fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+    const country = await response.json()
+    setCountry(country)
+    console.log(country)
+  }
+
+    fetchCountry()
+ } , [])
+
   return (
     <>
+    <Link to="/" className='btn btn-light'>
+      <i className="fas fa-arrow-left"></i>Back Home
+    </Link>
     <h1>Country Data</h1>
     </>
   )
